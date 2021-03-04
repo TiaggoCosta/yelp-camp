aa19f10d58346b068d1278799427e6cbaf3ded96
Tiago Costa <tiago.tagcosta@gmail.com>
2021-03-04 18:04:03 -0300

    add favicon

--- app.js
+++ app.js
@@ -160,7 +160,7 @@ app.use((err, req, res, next) => {
 })
 
 const port = process.env.PORT || 3000;
-app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log(`Serving on port ${port}`)
 })
 
