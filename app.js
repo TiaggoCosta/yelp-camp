b773f62cfa63762a720ced3c842fa491acbbce0d
Tiago Costa <tiago.tagcosta@gmail.com>
2021-03-04 18:07:55 -0300

    remove port

--- app.js
+++ app.js
@@ -160,7 +160,7 @@ app.use((err, req, res, next) => {
 })
 
 const port = process.env.PORT || 3000;
-app.listen(process.env.PORT || 3000, () => {
+app.listen(process.env.PORT, () => {
     console.log(`Serving on port ${port}`)
 })
 
